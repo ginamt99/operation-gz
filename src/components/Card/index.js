a4953fca0bfff736c80cc6b3e24a3da69e30745c
burcarz <zacharyburcar@gmail.com>
2023-06-11 20:49:20 -0400

    card changes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,10 +2,21 @@ import react, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const CardComp = ({ className, name, value }) => {
+
+    // const dragStart = () => {
+    //     console.log('started dragging');
+    //     setTimeout(() => this.className = 'card-selected', 0);
+    // }
+
+    // const dragEnd = () => {
+    //     console.log('ended dragging');
+    //     setTimeout(() => this.className = 'card-played', 0);
+    // }
+
     return (
         <>
         <div className={className}>
-            <div className="card-container">
+            <div className="card-container" draggable={true} onDragStart={dragStart} onDragEnd={dragEnd}>
                 <h1>{name}</h1>
                 <p>{value}</p>
             </div>
